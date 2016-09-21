facbacf735dad7955776b58b2be131a95cd92245
Kishan Wanigasinghe <kwanigasinghe@quicksolutions.com>
2016-09-20 20:50:27 -0400

    socket io init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use("/css", express.static(__dirname + '/css'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.htm');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
