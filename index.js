71a0e81ab52f1fb36ff00d3ec1fdf98d2900700d
Kishan Wanigasinghe <kwanigasinghe@quicksolutions.com>
2016-09-21 10:42:48 -0400

    app is working, yayy

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
