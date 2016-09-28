2bc3d2a3515250631a7579b17ed4039dc8de6f47
Kishan Wanigasinghe <kwanigasinghe@quicksolutions.com>
2016-09-27 22:23:10 -0400

    chat app is alive

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use("/css", express.static(__dirname + '/css'));
+app.users = [];
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.htm');
+  res.sendFile(__dirname + '/login.htm');
+});
+
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/chat.htm');
 });
 
 http.listen(3000, function(){
@@ -15,11 +20,19 @@ http.listen(3000, function(){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    var message = msg.userName + " says: " + msg.message;
+    io.emit('chat message', message);
   });
+
+  socket.on('new user', function(userName){
+    app.users[app.users.length] = userName;
+    io.emit('chat message', userName + " joined. Now we have " + app.users.length + " onilne.");
+  });
+
 });
